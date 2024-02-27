97b537f25cfd2b27a7507b3e35d309b105bcbeb3
Ingrid Guyedes <124937083+IngridGuyedes@users.noreply.github.com>
2024-02-27 13:54:13 -0600

    Semana 4

---
+++ Guia04/cart-react/src/app/data.js
@@ -0,0 +1,69 @@
+export const data=[
+{
+id:1,
+title:'Cien años de soledad',
+price:100,
+urlImage:'https://images.penguinrandomhouse.com/cover/9780525562443',
+quantity:1
+},
+{
+id:2,
+title:'El señor de los anillos(Trilogía)',
+price:190,
+urlImage:'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+quantity:1
+},
+{
+id:3,
+title:'Cuentos de Barro',
+price:30,
+urlImage:'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+quantity:1
+},
+{
+id:4,
+title:'Tierra de Infancia',
+price:30,
+urlImage:'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
+quantity:1
+}, {
+id:5,
+title:'Harry Potter La Piedra Filosofal',
+price:75,
+urlImage:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS9u-VNBlLcKIFF_UeDq40FlCO6F1y3pEBuiJTe48SDL4QclmIt',
+quantity:1
+},{
+id:6,
+title:'Harry Potter La Camara de los Secretos',
+price:75,
+urlImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG3ZGXW_S2_JozCXQXbFsvGmdyfGFnc_dulr7AMDbYoOipuHTW',
+quantity:1
+},
+{
+id:7,
+title:'Harry Potter y El Prisionero de Azkaban',
+price:75,
+urlImage:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ0Qo7KP46XwoywveaWJ_3R2UAKPf1hblVLJz_rjvXHokWSVgBS',
+quantity:1
+},
+{
+id:8,
+title:'Harry Potter y el Cáliz de Fuego',
+price:75,
+urlImage:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQX8Hb7M7FVW21NJNacV8Tkbp7Fb9sVNwxj5P9ukY7tSgr7s-6L',
+quantity:1
+},
+{
+id:9,
+title:'Harry Potter y la Orden del Fénix',
+price:75,
+urlImage:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSZJ-3Jb5Txu2QayqrJm9V2V3Y_WeY25enb5deZG9ACuO3SzlpE',
+quantity:1
+}, {
+id:10,
+title:'Harry Potter y el Misterio del Principe',
+price:75,
+urlImage:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQRJ_lcfmthTGk1pvkpCv9VhQFzFKMWvMxO67KtxKz5JZRa0so0',
+quantity:1
+}
+];
